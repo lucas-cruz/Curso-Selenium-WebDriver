96608411bcab06451df5af1249f1ba84d5a25054
Lucas Cruz <lucas030795@hotmail.com>
2024-07-24 22:41:25 -0300

    configuração da inicialização do driver pela pasta do projeto

--- src/main/java/TesteCampoTreinamento.java
+++ src/main/java/TesteCampoTreinamento.java
@@ -1,18 +1,16 @@
-import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class TesteGoogle {
+public class TesteCampoTreinamento {
 	
 	@Test
 	public void teste() {
-		//direcionamento para o driver alocado no diretório do projeto
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
-		
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.google.com");
-		Assert.assertEquals("Google", driver.getTitle());
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		
+		
 		driver.quit();
 	}
 
